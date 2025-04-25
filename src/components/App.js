1e18f7d5b217e2f1fea4017f3dcdcc6e8a1d6191
Fredrick Okoth <fredrick.okoth1@student.moringaschool.com>
2025-04-25 07:08:06 +0300

    components-props-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Header from "./Header";
+//
+import ArticleList from "./ArticleList";
+import About from "./About";
 import blogData from "../data/blog";
 
 console.log(blogData);
@@ -8,6 +12,9 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
